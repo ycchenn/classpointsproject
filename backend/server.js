2cceb2bc193eca1b8a85f233bc84f2502d731d50
ycchenn <gigi940317@gmail.com>
2026-06-25 18:17:15 +0800

    fix: 新增 Vercel 網址到 CORS

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ app.use(cors({
     'http://localhost:5174',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
-    'https://ycchenn.github.io'
+    'https://ycchenn.github.io',
+    'https://classpointsproject.vercel.app'
   ]
 }))
 app.use(express.json());
